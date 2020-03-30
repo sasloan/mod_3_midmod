cb5126ad863ab8c175e799a743458b8d3061bc60
sasloan <sebastiansloan94@gmail.com>
2020-03-30 09:37:31 -0600

    Create rough draft of spec

---
+++ spec/features/search/ingredient_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'As a Visitor' do
+	describe 'I can search ingredients' do
+		it 'And recieve dishes with the ingredient in it', :vcr do
+
+			visit '/'
+
+			select 'sweet potatoes', from: :ingredients
+
+			click_on "search"
+
+			expect(current_path).to eq("/foods")
+
+			expect(page).to have_content("7 results")
+			expect(page).to have_css(".food", count: 7)
+
+			within(first(".food")) do
+				expect(page).to have_css(".GTIN/UPC code")
+				expect(page).to have_css(".description")
+				expect(page).to have_css(".Brand Owner")
+				expect(page).to have_css(".ingredients")
+			end
+		end
+	end
+end
